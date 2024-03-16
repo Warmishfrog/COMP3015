ba4aadb65851d1e5f153b3b7f6cdedfa0c72fcf8
Warmishfrog <warmishfrog@gmail.com>
2024-03-16 16:14:08 +0000

    ignore ipch

---
+++ scenebasic_uniform.cpp
@@ -0,0 +1,169 @@
+#include "scenebasic_uniform.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <string>
+using std::string;
+
+#include <sstream>
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+#include "helper/glutils.h"
+#include "helper/texture.h"
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat3;
+using glm::mat4;
+
+SceneBasic_Uniform::SceneBasic_Uniform() :
+	tPrev(0.0f),
+	angle(0),
+	torus(2.5f * 2.0f, 0.3f * 2.0f, 50, 10), //(float MajorRadius, float MinorRadius, int numMajor, int numMinor)
+	plane(50.0f, 50.0f, 1, 1) //(float xsize, float zsize, int xdivs, int zdivs)
+{
+	mesh = ObjMesh::load("media/Skeleton/Skelly.obj", true); //load custom model here
+}
+
+void SceneBasic_Uniform::initScene()
+{
+	compile();
+	glEnable(GL_DEPTH_TEST);
+	model = mat4(1.0f);
+	view = glm::lookAt(
+		vec3(3.0f, 3.0f, 4.0f),
+		vec3(0.0f, 0.75f, 0.0f),
+		vec3(0.0f, 1.0f, 0.0f));
+	projection = mat4(1.0f);
+
+	GLuint Rock = Texture::loadTexture("media/texture/cement.jpg");
+	GLuint Lava = Texture::loadTexture("media/texture/fire.png");
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, Rock);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, Lava);
+
+
+	//generate 3 lights
+		float x, z;
+		for (int i = 0; i < 3; i++) {  
+			std::stringstream name;
+			name << "lights[" << i << "].Position";
+			x = 2.0f * cosf((glm::two_pi<float>() / 3) * i);
+			z = 2.0f * sinf((glm::two_pi<float>() / 3) * i);
+			prog.setUniform(name.str().c_str(), view * glm::vec4(x, 1.2f, z + 1.0f, 1.0f));
+		}
+
+		//intensity
+		prog.setUniform("lights[0].L", vec3(0.0f,0.0f, 0.8f));
+		prog.setUniform("lights[1].L", vec3(0.0f, 0.8f, 0.0f));
+		prog.setUniform("lights[2].L", vec3(0.8f, 0.0f, 0.0f));
+
+		//ambient
+		prog.setUniform("lights[0].La", vec3(0.0f, 0.0f, 0.2f));
+		prog.setUniform("lights[1].La", vec3(0.0f, 0.2f, 0.0f));
+		prog.setUniform("lights[2].La", vec3(0.2f, 0.0f, 0.0f));
+
+		//spotlight
+		prog.setUniform("Spot.L", vec3(0.9f));
+		prog.setUniform("Spot.La", vec3(0.5f));
+		prog.setUniform("Spot.Exponent", 50.0f);
+		prog.setUniform("Spot.Cutoff", glm::radians(15.0f));
+
+	//The Fog is coming
+		prog.setUniform("Fog.MaxDist", 10.0f);
+		prog.setUniform("Fog.MinDist", 1.0f);
+		prog.setUniform("Fog.Color", vec3(0.6f, 0.6f, 0.6f)); //RGB
+		
+}
+
+void SceneBasic_Uniform::compile()
+{
+	try {
+		prog.compileShader("shader/basic_uniform.vert");
+		prog.compileShader("shader/basic_uniform.frag");
+		prog.link();
+		prog.use();
+	} catch (GLSLProgramException &e) {
+		cerr << e.what() << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void SceneBasic_Uniform::update( float t )
+{
+	//spin
+	float deltaT = t - tPrev;
+	if (tPrev == 0.0f) deltaT = 0.0f;
+	tPrev = t;
+	angle += 0.25f * deltaT;
+	if (angle > glm::two_pi<float>()) angle -= glm::two_pi<float>();
+}
+
+void SceneBasic_Uniform::render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	
+	//Render Spotlight
+		vec4 lightPos = vec4(10.0f * cos(angle), 10.0f, 10.0f * sin(angle), 1.0f);
+		prog.setUniform("Spot.Position", vec3(view * lightPos));
+		mat3 normalMatrix = mat3(vec3(view[0]), vec3(view[1]), vec3(view[2]));
+		prog.setUniform("Spot.Direction", normalMatrix * vec3(-lightPos));
+		/**/
+
+	// Render the plane
+		prog.setUniform("Material.Kd", vec3(0.1f, 0.1f, 0.1f));
+		prog.setUniform("Material.Ka", vec3(0.7f, 0.7f, 0.7f));
+		prog.setUniform("Material.Ks", vec3(0.9f, 0.9f, 0.9f));
+		prog.setUniform("Material.Shininess", 180.0f);
+
+		model = mat4(1.0f);
+		model = glm::translate(model, vec3(0.0f, -0.45f, 0.0f));
+		setMatrices();
+		plane.render();
+
+	// Render the mesh
+		prog.setUniform("Material.Kd", vec3(0.5f, 0.5f, 0.5f));
+		prog.setUniform("Material.Ka", vec3(0.9f, 0.5f, 0.2f) * 0.1f);
+		prog.setUniform("Material.Ks", vec3(0.95f, 0.95f, 0.95f));
+		prog.setUniform("Material.Shininess", 100.0f);
+
+		model = mat4(1.0f);
+		model = glm::rotate(model, glm::radians(45.0f), vec3(0.0f, 1.0f, 0.0f));
+		model = glm::translate(model, vec3(0.0f, 0.5f, 0.0f));
+		model = glm::scale(model, vec3(3.0f));
+		setMatrices();
+		mesh->render();
+
+	// Render the torus
+		prog.setUniform("Material.Kd", vec3(0.9f, 0.9f, 0.9f));
+		prog.setUniform("Material.Ka", vec3(0.5f, 0.5f, 0.5f));
+		prog.setUniform("Material.Ks", vec3(0.1f, 0.1f, 0.1f));
+		prog.setUniform("Material.Shininess", 010.0f);
+
+		model = mat4(1.0f);
+		model = glm::rotate(model, glm::radians(90.0f), vec3(1.0f, 0.0f, 0.0f));
+		setMatrices();
+		torus.render();
+
+}
+
+void SceneBasic_Uniform::resize(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    width = w;
+	height = h;
+	projection = glm::perspective(glm::radians(70.0f), (float)w / h, 0.3f, 100.0f);
+}
+
+void SceneBasic_Uniform::setMatrices()
+{
+	mat4 mv = view * model;
+	prog.setUniform("ModelViewMatrix", mv);
+	prog.setUniform("NormalMatrix", glm::mat3(vec3(mv[0]), vec3(mv[1]), vec3(mv[2])));
+	prog.setUniform("MVP", projection * mv);
+}
---
+++ scenebasic_uniform.h
@@ -0,0 +1,41 @@
+#ifndef SCENEBASIC_UNIFORM_H
+#define SCENEBASIC_UNIFORM_H
+
+#include "helper/scene.h"
+
+#include <glad/glad.h>
+#include "helper/glslprogram.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "helper/torus.h"
+#include "helper/teapot.h"
+#include "glm/glm.hpp"
+#include "helper/plane.h"
+#include "helper/objmesh.h"
+
+class SceneBasic_Uniform : public Scene
+{
+private:
+    float tPrev;
+    float angle;
+
+    Plane plane;
+    Torus torus;
+    std::unique_ptr<ObjMesh> mesh;
+
+    GLuint Rock, Lava;
+
+    GLSLProgram prog;
+    void setMatrices();
+    void compile();
+
+public:
+    SceneBasic_Uniform();
+
+    void initScene();
+    void update( float t );
+    void render();
+    void resize(int, int);
+};
+
+#endif // SCENEBASIC_UNIFORM_H
